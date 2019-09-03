6beb621e9e8a55bbaf62f0cd8e44a603c2c4b496
Roman Shershnev <avel.leypx@gmail.com>
2019-09-04 00:52:30 +0300

    промисы с ручными данными

--- src/js/promise.js
+++ src/js/promise.js
@@ -1,1 +1,66 @@
-//делает Рома
+const passportControl = new Promise(function (resolve, reject) {
+    let passport = true;
+    let age = 22;
+
+    if (passport == true && age > 16) {
+        resolve({
+            visa : true
+        });
+
+    } else {
+        reject("Не хватает пасспорта или допустимого возраста");
+    }
+    });
+    passportControl.then(function(value) {
+        console.log("Пасспортный контроль пройден, Visa:", JSON.stringify(value));
+    });
+    passportControl.catch(function(reason) {
+        console.log("Пасспортный контроль не пройден.", reason);
+    });
+
+
+const medicalExamination = new Promise(function (resolve, reject) {
+    let healthy = 65;
+
+        if (healthy > 58) {
+            resolve({
+                visa : true
+            });
+
+        } else {
+            reject("Состояние здоровья не позволяет.");
+        }
+    });
+    medicalExamination.then(function(value) {
+        console.log("Медосмотр пройден, Visa:", JSON.stringify(value));
+    });
+    medicalExamination.catch(function(reason) {
+        console.log("Медосмотр не пройден", reason);
+    });
+
+
+const bank = new Promise(function (resolve, reject) {
+        let money = 20000;
+        if (money > 25000) {
+            resolve({
+                visa : true
+            });
+
+        } else {
+            reject("Недостаточно денег на личном счёте");
+        }
+    });
+    bank.then(function(value) {
+            console.log("Банк одобрил, Visa:", JSON.stringify(value));
+        });
+    bank.catch(function(reason) {
+            console.log("Банк не одобрил", reason);
+        });
+
+Promise.all([passportControl, medicalExamination, bank]).then(() => {
+    console.log('Все инстанции пройдены!!! Поздравляем!!!')
+        })
+        .catch (() => {
+            console.log('Что-то пошло не так, виза не получена!!')
+        })
+

ad2104566190c69ef960b673647f3213ccf93194
Roman Shershnev <avel.leypx@gmail.com>
2019-09-04 01:10:26 +0300

    промисы с ручными данными + setTimeout

--- src/js/promise.js
+++ src/js/promise.js
@@ -1,66 +1,81 @@
 const passportControl = new Promise(function (resolve, reject) {
-    let passport = true;
-    let age = 22;
 
-    if (passport == true && age > 16) {
-        resolve({
-            visa : true
+    setTimeout(() => {
+        let passport = true;
+        let age = 22;
+
+        if (passport == true && age > 16) {
+            resolve({
+                visa: true
+            });
+
+        } else {
+            reject("Не хватает пасспорта или допустимого возраста");
+        }
+
+        passportControl.then(function (value) {
+            console.log("Пасспортный контроль пройден, Visa:", JSON.stringify(value));
         });
-
-    } else {
-        reject("Не хватает пасспорта или допустимого возраста");
-    }
-    });
-    passportControl.then(function(value) {
-        console.log("Пасспортный контроль пройден, Visa:", JSON.stringify(value));
-    });
-    passportControl.catch(function(reason) {
-        console.log("Пасспортный контроль не пройден.", reason);
-    });
-
+        passportControl.catch(function (reason) {
+            console.log("Пасспортный контроль не пройден.", reason);
+        });
+    }, 2000);
+});
 
 const medicalExamination = new Promise(function (resolve, reject) {
-    let healthy = 65;
+
+
+    setTimeout(() => {
+        let healthy = 65;
 
         if (healthy > 58) {
             resolve({
-                visa : true
+                visa: true
             });
 
         } else {
             reject("Состояние здоровья не позволяет.");
         }
-    });
-    medicalExamination.then(function(value) {
+    medicalExamination.then(function (value) {
         console.log("Медосмотр пройден, Visa:", JSON.stringify(value));
     });
-    medicalExamination.catch(function(reason) {
+    medicalExamination.catch(function (reason) {
         console.log("Медосмотр не пройден", reason);
     });
+},4000);
+});
 
 
-const bank = new Promise(function (resolve, reject) {
-        let money = 20000;
-        if (money > 25000) {
-            resolve({
-                visa : true
+
+    const bank = new Promise(function (resolve, reject) {
+
+        setTimeout(() => {
+            let money = 20000;
+            if (money > 25000) {
+                resolve({
+                    visa: true
+                });
+
+            } else {
+                reject("Недостаточно денег на личном счёте");
+            }
+
+            bank.then(function (value) {
+                console.log("Банк одобрил, Visa:", JSON.stringify(value));
             });
+            bank.catch(function (reason) {
+                console.log("Банк не одобрил", reason);
+            });
+        }, 2000);
+    })
 
-        } else {
-            reject("Недостаточно денег на личном счёте");
-        }
-    });
-    bank.then(function(value) {
-            console.log("Банк одобрил, Visa:", JSON.stringify(value));
-        });
-    bank.catch(function(reason) {
-            console.log("Банк не одобрил", reason);
-        });
 
-Promise.all([passportControl, medicalExamination, bank]).then(() => {
-    console.log('Все инстанции пройдены!!! Поздравляем!!!')
+
+        Promise.all([passportControl, medicalExamination, bank]).then(() => {
+            console.log('Все инстанции пройдены!!! Поздравляем!!!')
         })
-        .catch (() => {
-            console.log('Что-то пошло не так, виза не получена!!')
-        })
+            .catch(() => {
+                console.log('Что-то пошло не так, виза не получена!!')
+            })
+
 
